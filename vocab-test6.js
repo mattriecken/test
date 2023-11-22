d8dbbcb8c9617e66d8924f802a22ddb93edc302b
Matthew Riecken <mcriecken@gmail.com>
2023-11-22 11:10:15 -0600

    Create vocab-test6.js

---
+++ vocab-test6.js
@@ -0,0 +1,31 @@
+var MYLIBRARY = MYLIBRARY || (function(){
+	
+	let vocabContainers = document.getElementsByClassName('lk-vocab');
+	for(const vocabContainer in vocabContainers) {
+		vocabContainer.style.padding = "25px";
+		vocabContainer.style.borderRadius = "4px";
+		vocabContainer.style.background = "lightblue";
+		vocabContainer.children[0].style.fontWeight = "700";
+		vocabContainer.children[1].style.fontSize = ".5em";
+	}
+	
+	
+    // var _args = {}; // private
+//
+//     return {
+//         init : function(Args) {
+//             _args = Args;
+//             // some other initialising
+//         },
+//         (function() {
+// 			let vocabContainers = document.getElementsByClassName('lk-vocab');
+// 			for(const vocabContainer in vocabContainers) {
+// 				vocabContainer.style.padding = "25px";
+// 				vocabContainer.style.borderRadius = "4px";
+// 				vocabContainer.style.background = "lightblue";
+// 				vocabContainer.children[0].style.fontWeight = "700";
+// 				vocabContainer.children[1].style.fontSize = ".5em";
+// 			}
+//         })();
+//     };
+}());
