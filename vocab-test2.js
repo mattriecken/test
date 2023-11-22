fd35515a4940f537ed984e765649f0e6593d9ca5
Matthew Riecken <mcriecken@gmail.com>
2023-11-22 08:59:52 -0600

    Add files via upload

---
+++ vocab-test2.js
@@ -0,0 +1,17 @@
+var MYLIBRARY = MYLIBRARY || (function(){
+    var _args = {}; // private
+
+    return {
+        init : function(Args) {
+            _args = Args;
+            // some other initialising
+        },
+        helloWorld : function() {
+			let div = document.createElement('div');
+			div.classList.add('lk-test');
+			let text = document.createTextNode('Hey this is my new paragraph.' + _args[0]);
+			div.appendChild(text);
+			document.body.appendChild(div);
+        }
+    };
+}());
